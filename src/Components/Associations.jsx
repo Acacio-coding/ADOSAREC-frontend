62c67f511447ede8c7eb69c5269a5fae2dc1faf3
Acacio-coding <acacio.coding@gmail.com>
2021-09-22 14:43:34 -0300

    Criação de estrutura e arquivos básicos do projeto

---
+++ src/Components/Associations.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Associations = () => {
+  return <></>;
+};
+
+export default Associations;
---
+++ src/Components/Donations.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Donations = () => {
+  return <></>;
+};
+
+export default Donations;
