f060fc2582710e1b6b7185c0943879b85773e0db
Acacio-coding <acacio.coding@gmail.com>
2021-09-23 01:32:07 -0300

    Adicionando pastas necessárias

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Statistics = () => {
+  return (
+    <>
+      <div className="statisticfull">
+        <div className="statisticSideBar">
+          <div className="">
+            <h1>Estatísticas</h1>
+            <h1>Doadores</h1>
+            <h1>Doações</h1>
+            <h1>Associações</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Statistics;
